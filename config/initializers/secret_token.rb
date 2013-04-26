42cea4bd2e8354cfc618e1de79f753f38c8a26f4
jennyyuejin <jin.yuejenny@gmail.com>
2013-04-25 17:21:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicofDeduction::Application.config.secret_token = '161e2df07ca0f4da228480a999183cc3ba628b00fa5d42872d723544558ba390d2634b3df018b85debdc28c350e684090e8b7887e1ac4f55ad1067aa368d829d'
